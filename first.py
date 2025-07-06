d4b099b4f9b3b7e2b74352cabf81912ff706881d
Leo Joy <109720012+leo-jr-hah@users.noreply.github.com>
2025-07-06 15:54:22 +0530

    Update first.py

--- first.py
+++ first.py
@@ -3,7 +3,7 @@
 #
 # This is a Blender script that uses procedural generation to create
 # textured 3D spaceship models. Tested with Blender 2.77a
-#sample hello world how to use this file to  
+#sample hello world how to use this file 
 
 import sys
 import os
